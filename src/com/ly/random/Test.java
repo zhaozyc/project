b90061b4415096455bf81e3ac4e4a78f1ca4fed2
词中素描 <38837783+zhaozyc@users.noreply.github.com>
2020-02-12 13:39:42 +0800

    第一次提交 短信接收

---
+++ src/com/ly/random/Test.java
@@ -0,0 +1,12 @@
+package com.ly.random;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RandomNum od=new RandomNum();
+		String num=od.getRandom();
+		System.out.println(num);
+	}
+
+}
